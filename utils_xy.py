f15e54058d672da3cb3211044ee3f07257119e57
xysong1201 <xiaoyu.song2013@gmail.com>
2019-01-24 17:20:31 +0800

    24/01

--- utils_xy.py
+++ utils_xy.py
@@ -9,3 +9,13 @@ def display_num_param(net):
     print('There are {} ({:.2f} million) parameters in this neural network'.format(
         nb_param, nb_param/1e6)
          )
+    
+def get_error( scores , labels ):
+
+    bs=scores.size(0)
+    predicted_labels = scores.argmax(dim=1)
+    indicator = (predicted_labels == labels)
+    num_matches=indicator.sum()
+    
+    return 1-num_matches.float()/bs    
+
